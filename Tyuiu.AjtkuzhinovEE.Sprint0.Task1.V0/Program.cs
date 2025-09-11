65a0b5b1aee12906c41207b37c0875d180f06051
admin <admin@DESKTOP-GKKSMO3>
2025-09-11 14:14:40 +0300

    Task1

---
+++ Tyuiu.AjtkuzhinovEE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World");
+        string name = " Елжас";
+        int age = 17;
+        Console.WriteLine("Здравствуйте меня зовут" + name + " мне " + age + " лет! ");
+        Console.ReadKey();
+    }
+}
