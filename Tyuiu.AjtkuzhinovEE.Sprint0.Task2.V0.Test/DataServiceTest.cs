8f3059e8a7b0fcc1306c21e4fae284de5f704669
admin <admin@DESKTOP-GKKSMO3>
2025-09-12 07:02:16 +0300

    Task2

---
+++ Tyuiu.AjtkuzhinovEE.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.AjtkuzhinovEE.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создание методов тестирования,методов из библиотеки
+            var name = "Елжас";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Asser и метод AreEqual
+            Assert.AreEqual("Привет..., Елжас", res);
+        }
+    }
+}
