096adb5089c2dd92792affe8392245c55d4825ac
admin <admin@DESKTOP-GKKSMO3>
2025-09-12 20:41:33 +0300

    Task4

---
+++ Tyuiu.AjtkuzhinovEE.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿namespace Tyuiu.AjtkuzhinovEE.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        //Пример линейной структуры(плюс)
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        //Пример линейной структуры(минус)
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        //Пример линейной структуры(умножить)
+        public static int Multiplication(int a, int b)
+        { 
+          return (a * b);
+        }
+
+        //Пример линейной структуры(деление)
+        public static double Division(int a, int b)
+        {
+            return a / b;
+        }
+    
+    }
+}
