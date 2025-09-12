2c3176f4bee669162731ef6051db7526e2e44741
admin <admin@DESKTOP-GKKSMO3>
2025-09-12 09:06:47 +0300

    Task3

---
+++ Tyuiu.AjtkuzhinovEE.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Tyuiu.AjtkuzhinovEE.Sprint0.Task3.V0.Lib;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Sum(7, 7));
+        Console.ReadKey();
+
+    }   
+}
