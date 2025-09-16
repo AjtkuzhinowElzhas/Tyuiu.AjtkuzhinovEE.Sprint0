b92ca490c64818b63a7540f7c7135342b38c2624
admin <admin@DESKTOP-GKKSMO3>
2025-09-16 16:09:49 +0300

    Task5

---
+++ Tyuiu.AjtkuzhinovEE.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿//Подключение библиотеки
+using Tyuiu.AjtkuzhinovEE.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.AjtkuzhinovEE.Sprint0.Task5.V0
+{ 
+  class Program
+
+  {
+    static void Main(string[] args)
+    {
+        //Пример линейной структуры
+        Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+        Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+        Console.WriteLine("A * B + " + DataService.Multiplication(10, 10));
+
+        //Пример разветвляющейся структуры находится в библиотеке классов и методе Division
+        Console.WriteLine("A / B = " + DataService.Division(9, 3));
+        Console.ReadKey();
+
+    }
+  }
+}
+
+
